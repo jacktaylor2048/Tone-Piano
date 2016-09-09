0a491dcdb709a29a0330b415249a21d2c4651289
Jack Taylor <jack.taylor.2048@gmail.com>
2016-09-09 13:19:53 +0100

    Added additional constants

--- tone_piano/src/main.cpp
+++ tone_piano/src/main.cpp
@@ -1,4 +1,9 @@
+#include <conio.h>
 #include "note.h"
+
+const int CONSOLE_WIDTH = 64;
+const int CONSOLE_HEIGHT = 16;
+const unsigned char DEFAULT_NOTELENGTH = 150;
 
 // Clear console.
 void clear()
