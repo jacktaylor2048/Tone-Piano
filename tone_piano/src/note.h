2c3c5b14fe977d2d2a9b0cf8e4cf2fb4d6383127
Jack Taylor <jack.taylor.2048@gmail.com>
2016-09-09 16:26:04 +0100

    Moved Note constant explanations to block

--- tone_piano/src/note.h
+++ tone_piano/src/note.h
@@ -4,13 +4,17 @@
 #include <math.h>
 #include <windows.h>
 
-// Frequency of note A4.
+/* 
+ * BASE_FREQUENCY: Frequency of note A4.
+ * BASE_MULTIPLIER: Base value for which the semitone difference from the base 
+ * frequency will serve as the exponent (2 ^ 1/12).
+ * NOTES_PER_CONFIG: The amount of notes accessible for each octave configuration
+ * BASE_NOTE_DIFF: Semitone difference between base note (A) and lowest note for 
+ * a configuration (C)
+ */
 const double BASE_FREQUENCY = 440;
-// Base value for which the semitone difference from the base frequency will serve as the exponent (2 ^ 1/12).
 const double BASE_MULTIPLIER = 1.059463;
-// The amount of notes accessible for each octave configuration
 const int NOTES_PER_CONFIG = 31;
-// Semitone difference between base note (A) and lowest note for a configuration (C)
 const int BASE_NOTE_DIFF = 9;
 const int BASE_OCTAVE = 4;
 const int NOTES_PER_OCTAVE = 12;
