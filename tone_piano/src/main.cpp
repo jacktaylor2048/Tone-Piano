352627ea87a780324963a74d40e2a6532e2e0377
Jack Taylor <jack.taylor.2048@gmail.com>
2016-09-09 13:26:30 +0100

    Added initialisation sequence to main()

--- tone_piano/src/main.cpp
+++ tone_piano/src/main.cpp
@@ -5,20 +5,32 @@ const int CONSOLE_WIDTH = 64;
 const int CONSOLE_HEIGHT = 16;
 const unsigned char DEFAULT_NOTELENGTH = 150;
 
-// Clear console.
+// Clear console
 void clear()
 {
 
 }
 
-// Draw to console.
-void draw()
+// Draw to console
+void draw(bool& redraw)
 {
 
 }
 
-// Main function.
+// Main function
 int main()
 {
+	bool redraw = true;
+	bool playmode = false;
+	char key = 0;
+	char octave = BASE_OCTAVE;
+	unsigned char notelength = DEFAULT_NOTELENGTH;
+	srand(time_t(NULL));
+	// Initialise console window
+	SetConsoleTitle("Tone Piano");
+	SMALL_RECT window = {0, 0, CONSOLE_WIDTH - 1, CONSOLE_HEIGHT - 1};
+	COORD buffer = {CONSOLE_WIDTH, CONSOLE_HEIGHT};
+	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &window);
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), buffer);
 	return 0;
 }
