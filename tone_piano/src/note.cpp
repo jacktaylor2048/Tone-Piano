3ddedfce9e98850f767aa4befa6efc9ea002de7f
Jack Taylor <jack.taylor.2048@gmail.com>
2016-09-09 13:01:28 +0100

    Added note.cpp definitions

---
+++ tone_piano/src/note.cpp
@@ -0,0 +1,24 @@
+#include "note.h"
+// Constructor.
+Note::Note()
+{
+
+}
+
+// Play this note.
+void Note::play(char& octave, unsigned char& notelength)
+{
+
+}
+
+// Set a random note.
+void Note::set_note()
+{
+
+}
+
+// Set a specific note based on key press.
+void Note::set_note(char key)
+{
+
+}
